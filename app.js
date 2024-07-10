723e32be2367e8f4cee05a39349c83d14b86e583
muntazirmalikshigri <muntazirshigri.12@gmail.com>
2024-07-10 05:39:21 +0500

    this is ready

---
+++ app.js
@@ -0,0 +1,24 @@
+// let boy1="malik";
+// console.log(boy1);
+// console.log(boy1.trim());
+// console.log(boy1[0]);
+// for(let i=0;i<boy1.length;i++){
+//     console.log(boy1[i]);
+// }
+let name=()=>{
+    console.log("malik jee");
+}
+
+// let boy2="munna";
+// let sentence=`${boy1} is a friend of ${boy2}`
+// console.log(sentence);
+// let fruit='Bana\'na';
+// console.log(fruit);
+// console.log(fruit.length);
+
+// console.log(fruit.toUpperCase());
+// console.log(fruit.toLowerCase());
+// console.log(fruit.toLocaleUpperCase());
+// // console.log(fruit.slice(2));
+// console.log(fruit.replace("Ba","Coco"));
+// console.log(boy1.concat(` is friend of ${boy2} okay`));
